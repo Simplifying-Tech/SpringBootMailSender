862086917fc86de403a4b993b978ed240456de71
Sushil Patil <40490417+patilsushil1001@users.noreply.github.com>
2021-11-22 11:45:18 +0530

    Add files via upload

---
+++ src/main/java/com/simplifying/tech/SpringbootMailSender/SpringbootMailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.simplifying.tech.SpringbootMailSender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplifying/tech/SpringbootMailSender/SpringbootMailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplifying.tech.SpringbootMailSender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
